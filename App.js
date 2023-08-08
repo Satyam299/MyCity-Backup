0b63c54646e5d536f0b3a63dde3a42d4ea53ba54
Satyam299 <108457105+Satyam299@users.noreply.github.com>
2023-08-08 07:51:46 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import { StyleSheet,Alert , Text, View } from 'react-native'
+import React , { useEffect } from 'react'
+import Nav from './navigation/Nav'
+
+const App = () => {
+
+  return (
+    <Nav />
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
